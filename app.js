0d2a6cf33f589b5f776583a35625a036a5d97e2f
unknown <jemahechaj@ut.edu.co>
2019-03-01 12:41:22 -0500

    probando ajax

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-$(function () {
-    console.log("Jquery Ok");
+$(document).ready(function () {
+    /* Guardando lo que esta en el formulario*/
+   $('#search').keyup(function(e){
+        let search = $('#search').val();
+        $.ajax({
+            url: 'task-search.php',
+            type: 'POST',
+            data: {search}, //Enviando valor de input
+            success: function (response) {
+                console.log(response);
+            }
+        })
+        //console.log(search);
+   }); 
+
 });
--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
-    $conection = mysqli_connect(
+    $connection = mysqli_connect(
         'localhost',
         'root',
         '',
         'task_app'
     );
 
-    if ($conection) {
+    if ($connection) {
         echo "ok";
     }
 
